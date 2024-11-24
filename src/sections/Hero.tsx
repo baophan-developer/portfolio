f1f70c4436aff9b37927e973ea8d09e14bfeb978
Bao Phan <baophan.developer@gmail.com>
2024-11-24 23:58:16 +0700

    feature [BaoPH] hero section add content

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,43 @@
+import memojiImage from "@/assets/images/memoji-computer.png";
+import ArrayDown from "@/assets/icons/arrow-down.svg";
+import Image from "next/image";
+
 export const HeroSection = () => {
-    return <div>Hero Section</div>;
+    return (
+        <div className="py-32">
+            <div className="container">
+                <div className="flex flex-col items-center">
+                    <Image
+                        src={memojiImage}
+                        alt="Person peeking from behind laptop"
+                        className="size-[100px]"
+                    />
+                    <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+                        <div className="bg-green-500 size-2.5 rounded-full"></div>
+                        <div className="text-sm font-medium">
+                            Available for new projects
+                        </div>
+                    </div>
+                </div>
+                <h1 className="font-serif text-3xl text-center mt-8 tracking-wide">
+                    Building Exceptional User Experiences
+                </h1>
+                <p className="mt-4 text-center text-white/60">
+                    I specialize in transforming designs into functional,
+                    high-performing web applications. Let's discuss your next
+                    project.
+                </p>
+                <div className="flex flex-col items-center mt-8 gap-4">
+                    <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+                        <span className="font-semibold">Explore My Work</span>
+                        <ArrayDown className="size-4" />
+                    </button>
+                    <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+                        <span>👋</span>
+                        <span className="font-semibold">Let's Connect</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
